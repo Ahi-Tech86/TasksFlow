98a0716075548441dca7616d0a3b2eb774b87f57
Ahi-Tech86 <ahi.tech86@gmail.com>
2024-11-04 18:46:41 +0300

    [Api-Gateway]: created Api Gateway and basic setup carried out

---
+++ ms-gateway/src/main/java/com/ahicode/GatewayRunner.java
@@ -0,0 +1,13 @@
+package com.ahicode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayRunner.class, args);
+    }
+}
