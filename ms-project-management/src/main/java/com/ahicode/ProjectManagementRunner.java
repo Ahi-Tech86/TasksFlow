5fc1dfb436710221b0085e2aee7f340fe78ec8cb
Ahi-Tech86 <ahi.tech86@gmail.com>
2024-11-06 23:12:43 +0300

    [Ms-Project]: created project microservice and added build.gradle

---
+++ ms-project-management/src/main/java/com/ahicode/ProjectManagementRunner.java
@@ -0,0 +1,19 @@
+package com.ahicode;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ProjectManagementRunner {
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().load();
+        dotenv.entries().forEach(
+                entry -> System.setProperty(entry.getKey(), entry.getValue())
+        );
+
+        SpringApplication.run(ProjectManagementRunner.class, args);
+    }
+}
